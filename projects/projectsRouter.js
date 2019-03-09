eec8707713a88ffb9c5c2e782533f595d6508481
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-03-08 19:46:16 -0700

    Add POST functionality to add new project

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const Projects = require('../data/helpers/projectModel');
 const router = express.Router();
 
+
+// GET all projects
 router.get('/', async (req, res) => {
   try {
     const projects = await Projects.get();
@@ -11,6 +13,21 @@ router.get('/', async (req, res) => {
   }
 })
 
+// POST, or add, new project
+router.post('/', async (req, res) => {
+  try {
+    const newProject = req.body;
+    if (newProject.name && newProject.description) {
+      const project = await Projects.insert(newProject);
+      res.status(201).json({newProject: project});
+    } else {
+      res.status(400).json({message: 'Please provide name and description of new project.'})
+    }
+    
+  } catch (err) {
+    res.status(500).json({err: 'There was a problem adding new project.'});
+  }
+})
 
 
 
