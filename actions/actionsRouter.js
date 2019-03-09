dba80f8cf92bd2797d70c4ed8b947204f6e73e6d
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-03-08 20:25:42 -0700

    Add PUT functionality to actions router

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -59,6 +59,24 @@ router.delete('/:id', async (req, res) => {
   }
 })
 
-
+//UPDATE, or edit, the action name and/or description
+router.put('/:id', async (req, res) => {
+  const {id} = req.params;
+  try {
+    const updatedAction = req.body;
+    const action = await Actions.update(id, req.body);
+    if (action){
+      if (updatedAction.project_id && updatedAction.description && updatedAction.notes) {
+        res.status(200).json({updatedAction: action});
+      } else {
+        res.status(400).json({err: "Please provide updated name and description for action"});
+      }
+    } else {
+      res.status(404).json({message: "The action with the specified ID does not exist."});
+    }
+  } catch (err) {
+    res.status(500).json({err: "Project failed to udpate."})
+  }
+})
 
 module.exports = router;
